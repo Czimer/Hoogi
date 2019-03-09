eb41e7a368dd86631f3f79fe27870925879ef7d7
Elad Motola <eladmotola95@gmail.com>
2019-03-09 18:00:47 +0200

    add expo-multiple-imagepicker

--- Hoogi/components/Feed/NewMessage.js
+++ Hoogi/components/Feed/NewMessage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { Subheading, Modal, TextInput, Portal, Button, Title, Card, IconButton } from 'react-native-paper';
-import { ImagePicker } from 'expo';
+import { ImageBrowser } from 'expo-multiple-imagepicker';
+import { Permissions } from 'expo'
 
 export default class Message extends React.Component {
     constructor(props) {
@@ -9,7 +10,8 @@ export default class Message extends React.Component {
 
         this.state = {
             newMessageText: '',
-            newPhotos: []
+            newPhotos: [],
+            imageBrowserOpen: false
         }
     }
 
@@ -21,14 +23,28 @@ export default class Message extends React.Component {
         this.props.onAccept(newMessageText, newPhotos)
     }
 
-    openGallery = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            base64: true
-        });
+    imageBrowserCallback = (callback) => {
+        callback.then((photos) => {
+            console.log(photos)
+            this.setState({
+                imageBrowserOpen: false,
+                newPhotos: photos
+            })
+        }).catch((e) => console.log(e))
+    }
 
-        this.setState(prevState => ({
-            newPhotos: [...prevState.newPhotos, result.base64]
-        }))
+    openGallery = async () => {
+        // let result = await ImagePicker.launchImageLibraryAsync({
+        //     base64: true
+        // });
+
+        // this.setState(prevState => ({
+        //     newPhotos: [...prevState.newPhotos, result.base64]
+        // }))
+        const perm = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (perm.status === 'granted') {
+            this.setState({ imageBrowserOpen: true })
+        }
     };
 
     onNewMessageChange = (text) => {
@@ -37,36 +53,41 @@ export default class Message extends React.Component {
 
     render() {
         const { isOpen, onClose } = this.props
-        const { newMessageText, newPhotos } = this.state
+        const { newMessageText, newPhotos, imageBrowserOpen } = this.state
+
+        if (imageBrowserOpen) {
+            return (<ImageBrowser max={10} callback={this.imageBrowserCallback} />);
+        }
+
         return (
-                <Portal>
-                    <Modal visible={isOpen} onDismiss={onClose} contentContainerStyle={styles.modal}>
-                        <Card>
-                            <Card.Content>
-                                <Title style={{ textAlign: 'right' }}>הודעה חדשה</Title>
-                                <IconButton
-                                    icon="add-a-photo"
-                                    size={20}
-                                    onPress={this.openGallery}
-                                />
-                                <TextInput
-                                    label="הודעה חדשה"
-                                    value={newMessageText}
-                                    multiline
-                                    numberOfLines={4}
-                                    onChangeText={this.onNewMessageChange}
-                                />
-                                {newPhotos.length > 0 &&
-                                    <Subheading style={{ textAlign: 'right' }}>{`נוספו ${newPhotos.length} תמונות`}</Subheading>
-                                }
-                            </Card.Content>
-                            <Card.Actions>
-                                <Button mode="contained" onPress={this.sendNewMessage}>שלח</Button>
-                                <Button style={{ marginLeft: 10 }} onPress={onClose}>בטל</Button>
-                            </Card.Actions>
-                        </Card>
-                    </Modal>
-                </Portal>
+            <Portal>
+                <Modal visible={isOpen} onDismiss={onClose} contentContainerStyle={styles.modal}>
+                    <Card>
+                        <Card.Content>
+                            <Title style={{ textAlign: 'right' }}>הודעה חדשה</Title>
+                            <IconButton
+                                icon="add-a-photo"
+                                size={20}
+                                onPress={this.openGallery}
+                            />
+                            <TextInput
+                                label="הודעה חדשה"
+                                value={newMessageText}
+                                multiline
+                                numberOfLines={4}
+                                onChangeText={this.onNewMessageChange}
+                            />
+                            {newPhotos.length > 0 &&
+                                <Subheading style={{ textAlign: 'right' }}>{`נוספו ${newPhotos.length} תמונות`}</Subheading>
+                            }
+                        </Card.Content>
+                        <Card.Actions>
+                            <Button mode="contained" onPress={this.sendNewMessage}>שלח</Button>
+                            <Button style={{ marginLeft: 10 }} onPress={onClose}>בטל</Button>
+                        </Card.Actions>
+                    </Card>
+                </Modal>
+            </Portal>
         )
     }
 }
