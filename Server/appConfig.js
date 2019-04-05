c649aa2571f8478991a00631d93bbd3173609f08
Elad Motola <eladmotola95@gmail.com>
2019-04-05 13:09:38 +0300

    add appcofig file

---
+++ Server/appConfig.js
@@ -0,0 +1,5 @@
+const config = {
+    ConnectionString:'postgres://ncaghsyivfudse:1ef06a45824934a1ae55f2690701ba6d874e359fb317f975e2f608bd9238badf@ec2-184-73-153-64.compute-1.amazonaws.com:5432/d4cvss8ktmk37i?sslmode=require&ssl=1'
+}
+
+module.exports = config
