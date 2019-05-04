a6324654db251ce46010634aa978e3bd8ce24cbc
Danash <danashak96@gmail.com>
2019-05-04 18:48:41 +0300

    added face recognition

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var face = require('./faceRecognition/faceRecognition-try')
 
 const { postgraphile } = require("postgraphile");
 const config = require('./appConfig')
