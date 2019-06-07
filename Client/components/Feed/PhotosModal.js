420f4eb05e777657bd3f771ecdcac76ea1ae0483
Elad Motola <eladmotola95@gmail.com>
2019-06-07 19:58:43 +0300

    move to function

--- Client/components/Feed/PhotosModal.js
+++ Client/components/Feed/PhotosModal.js
@@ -4,24 +4,28 @@ import appConfig from '../../appConfig';
 import { Portal, Modal, Card, Title } from 'react-native-paper';
 
 export default class PhotosModal extends React.Component {
+
+
+    renderListView = photosUri => {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        const dataSource = ds.cloneWithRows(photosUri)
+
+        return <ListView contentContainerStyle={{ flexDirection: 'row', flexWrap: 'wrap' }}
+            dataSource={dataSource}
+            pageSize={3}
+            renderRow={(data, rowID, sectionID) => (
+                <Image source={{ uri: data }} style={{ width: 125, height: 125, margin: 5 }} />)} />
+    }
+
     render() {
         const { onClose, photos, isOpen } = this.props
         const photosUri = photos.map(photo => { return `${appConfig.baseServerUrl}/${photo}` })
-        if(!photosUri.length) return <View></View>
-
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        const dataSource = ds.cloneWithRows(photosUri)
-
         return (
             <Portal>
                 <Modal visible={isOpen} onDismiss={onClose} contentContainerStyle={styles.modal}>
                     <Card>
-                        <Title style={{ textAlign: 'center' }}>תמונות</Title>                        
-                        <ListView  contentContainerStyle={{flexDirection:'row',flexWrap:'wrap'}}
-                                   dataSource={dataSource} 
-                                   pageSize={3}                    
-                                   renderRow={(data, rowID, sectionID) => (
-                                <Image  source={{ uri: data }} style={{ width: 125, height: 125, margin:5 }} />)} />                                    
+                        <Title style={{ textAlign: 'center' }}>תמונות</Title>
+                        {photosUri.length !== 0 && this.renderListView(photosUri)}
                     </Card>
                 </Modal>
             </Portal>
