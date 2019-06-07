c90bb51170e4244486b48068988ab1a01c1ce0f1
Elad Motola <eladmotola95@gmail.com>
2019-06-07 19:54:58 +0300

    fix showing pictures

--- Client/components/Feed/PhotosModal.js
+++ Client/components/Feed/PhotosModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet,View } from "react-native";
+import { Image, StyleSheet, View, ListView } from "react-native";
 import appConfig from '../../appConfig';
 import { Portal, Modal, Card, Title } from 'react-native-paper';
 
@@ -7,17 +7,21 @@ export default class PhotosModal extends React.Component {
     render() {
         const { onClose, photos, isOpen } = this.props
         const photosUri = photos.map(photo => { return `${appConfig.baseServerUrl}/${photo}` })
+        if(!photosUri.length) return <View></View>
+
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        const dataSource = ds.cloneWithRows(photosUri)
+
         return (
             <Portal>
                 <Modal visible={isOpen} onDismiss={onClose} contentContainerStyle={styles.modal}>
                     <Card>
-                        <Title style={{textAlign:'center'}}>תמונות</Title>
-                        <View style={{ flexDirection: 'row'}}>
-                        {photosUri.map(photo => {
-                            return <Image key={photo} source={{ uri: photo }} style={{ width: 150, height: 150 }} />
-
-                        })}
-                        </View>
+                        <Title style={{ textAlign: 'center' }}>תמונות</Title>                        
+                        <ListView  contentContainerStyle={{flexDirection:'row',flexWrap:'wrap'}}
+                                   dataSource={dataSource} 
+                                   pageSize={3}                    
+                                   renderRow={(data, rowID, sectionID) => (
+                                <Image  source={{ uri: data }} style={{ width: 125, height: 125, margin:5 }} />)} />                                    
                     </Card>
                 </Modal>
             </Portal>
@@ -27,9 +31,9 @@ export default class PhotosModal extends React.Component {
 
 const styles = StyleSheet.create({
     modal: {
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
-    card:{        
+    listView: {
         justifyContent: 'center',
         alignItems: 'center',
     }
