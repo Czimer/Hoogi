984ff399cb2b30e57168250d9d590c39114f2081
Danash <danashak96@gmail.com>
2019-06-17 21:46:30 +0300

    fixed directory in face recognition

--- Server/faceRecognition/faceRecognition.js
+++ Server/faceRecognition/faceRecognition.js
@@ -1,19 +1,13 @@
   var faceapi = require('face-api.js')
-
+  var path = require('path');
   var canvas = require('./commons/env')
   var faceDetection = require('./commons/faceDetection')
   var faceDetectionNet = faceDetection.faceDetectionNet
   var faceDetectionOptions = faceDetection.faceDetectionOptions
   var saveFileModule = require('./commons/saveFile')
-  var saveFile = saveFileModule.saveFile;
   var referenceImage;
-
-  const imagesDirectory = 'C:\\Users\\Dana\\Documents\\Users\\Dana\\ColmanCS\\FinalProject\\Hoogi\\Hoogi\\Server\\faceRecognition\\images\\'
-
-  const REFERENCE_IMAGE = imagesDirectory +  'onlydana.jpg'
-  const QUERY_IMAGE = imagesDirectory + 'danaandfriends.jpg'
-  const QUERY_IMAGE2 = imagesDirectory + 'imaShilhav.jpg'
-  const directory = 'C:\\Users\\Dana\\Documents\\Users\\Dana\\ColmanCS\\FinalProject\\Hoogi\\Hoogi\\Server\\faceRecognition\\weights'
+ 
+  const directory = path.join(__dirname, 'weights')
   var detectionResultsRefImg;
  
   async function loadAllNets(){
@@ -51,12 +45,6 @@
             .withFaceDescriptors()
             if(detectionResultsRefImg.length > 0 && detectionResultsQueryImg.length > 0){
               const faceMatcher = new faceapi.FaceMatcher(detectionResultsRefImg)
-
-              const labels = faceMatcher.labeledDescriptors.map(ld => ld.label)
-              
-              const refBoxesWithText = detectionResultsRefImg.map(res => res.detection.box).map((box, i) => new faceapi.BoxWithText(box, labels[i]))
-
-              const outRef = faceapi.createCanvasFromMedia(referenceImage)
               // Map the matches in the picture according to the face in the first picture which labeled as 'person 1'
               const bestMatchingFacesInQueryImagebla = detectionResultsQueryImg.map(currImgRes => {
                 return faceMatcher.findBestMatch(currImgRes.descriptor).label === 'person 1'       
