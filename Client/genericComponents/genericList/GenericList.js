72d3dc1d2b9c9fc7961f488fca97ef4a795f45f5
Elad Motola <eladmotola95@gmail.com>
2019-08-22 22:01:02 +0300

    remove warning

--- Client/genericComponents/genericList/GenericList.js
+++ Client/genericComponents/genericList/GenericList.js
@@ -3,55 +3,53 @@ import { Text, View, StyleSheet, ScrollView, TouchableHighlight } from 'react-na
 import { Card, HelperText } from 'react-native-paper';
 
 
-export default class GenericList extends Component{
-    constructor(props){
+export default class GenericList extends Component {
+    constructor(props) {
         super(props);
-        this.state = {          
+        this.state = {
             open: false
-        };    
+        };
     };
 
-    render(){
-        const {tableHead, tableData, handleLongPress, children} = this.props;       
-        return(
+    render() {
+        const { tableHead, tableData, handleLongPress, children } = this.props;
+        return (
             <>
                 <ScrollView>
-                {(tableData !== undefined) && tableData.map((row, rowIndex) =>{
-                    return(
-                        <Card style={styles.container}>
-                            <Card.Content>
-                                <TouchableHighlight key={rowIndex} onLongPress={(event) => handleLongPress(event, row)}>
-                                    <View>
-                                    { 
-                                        Object.keys(row).map((keyName, cellRowIndex) =>{
-                                            if(row[keyName] instanceof Array){
-                                                row[keyName] = row[keyName].join(', ')
+                    {(tableData !== undefined) && tableData.map((row, rowIndex) => {
+                        return (
+                            <Card style={styles.container} key={rowIndex}>
+                                <Card.Content>
+                                    <TouchableHighlight key={rowIndex} onLongPress={(event) => handleLongPress(event, row)}>
+                                        <View>
+                                            {
+                                                Object.keys(row).map((keyName, cellRowIndex) => {
+                                                    if (row[keyName] instanceof Array) {
+                                                        row[keyName] = row[keyName].join(', ')
+                                                    }
+                                                    return (
+                                                        <Text key={`${rowIndex} - ${cellRowIndex}`}>
+                                                            {tableHead[cellRowIndex]} :  {row[keyName]}
+                                                        </Text>
+                                                    )
+                                                })
                                             }
-                                            return (
-                                                <>
-                                                    <Text key={`${rowIndex} - ${cellRowIndex}`}>
-                                                        {tableHead[cellRowIndex]} :  {row[keyName]}
-                                                    </Text>
-                                                </>
-                                            )
-                                        })
-                                    }
-                                    </View>
-                                </TouchableHighlight>
-                            </Card.Content>
-                        </Card>
-                    );
-                })}
+                                        </View>
+                                    </TouchableHighlight>
+                                </Card.Content>
+                            </Card>
+                        );
+                    })}
                 </ScrollView>
 
-                {children}     
-        </>
+                {children}
+            </>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {borderRadius: 3,borderColor: '#9ED3F7', borderWidth: 1, backgroundColor: '#EEF8FF', margin: 8, marginBottom: 0},
-  });
+    container: { borderRadius: 3, borderColor: '#9ED3F7', borderWidth: 1, backgroundColor: '#EEF8FF', margin: 8, marginBottom: 0 },
+});
 
 
